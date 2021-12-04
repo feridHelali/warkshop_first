4ffadc7eafb67684b09a94a9cdb6abc065b52379
RA3-SIOUD <sioudlmo@gmail.com>
2021-12-04 10:36:07 +0100

    initial commit

---
+++ first_workshop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+int add(int a,int b){
+    printf("i m processing in add");
+    return a+b;
+}
+
+
+int main(){
+    printf("Hello :)\n");
+    printf("%d \n",add(5,2));
+    printf("End of program :)\n");
+
+    return 0;
+}
+
