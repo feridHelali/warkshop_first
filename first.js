7a3deff19b50257d21efac75f9538753a746084e
RA3-SIOUD <sioudlmo@gmail.com>
2021-12-10 15:07:59 +0100

    diff

---
+++ first.js
@@ -0,0 +1,15 @@
+a=5
+_name="Salem"
+isClosed=true
+aCar={brand:"Mercedes",color:"Matalic"}
+
+function greeting(message){
+    return 'Hello ' + message;
+}
+
+console.log(typeof a)
+console.log(typeof _name)
+console.log(typeof isClosed);
+console.log(typeof aCar)
+console.log(greeting)
+
--- first_workshop.cpp
+++ first_workshop.cpp
@@ -1,16 +1,31 @@
 #include<iostream>
 
+using namespace std;
 
 int add(int a,int b){
+
     printf("i m processing in add");
+    cout << "Hello "<< endl;
     return a+b;
 }
 
 
 int main(){
     printf("Hello :)\n");
-    printf("%d \n",add(5,2));
+    
+    int x=5,y=18;
+    int p=x*y;
+    
+    printf("%d \n",add(x,y));
+    printf("%d \n",p);
+
+     x=10;y=12;
+    int p2=x*y;
+
+    printf("%d \n",p2);
+
     printf("End of program :)\n");
+
 
     return 0;
 }
